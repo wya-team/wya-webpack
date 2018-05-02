0be54f56676cf14c3e0a328df304466eb2e18eb8
Deot <zrd0921@qq.com>
2018-05-03 02:37:22 +0800

    update webpack@4.6.0

--- config/webpack.config.dist.js
+++ config/webpack.config.dist.js
@@ -9,6 +9,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { APP_ROOT, commonConfig, localIp, localPort, libName } = require('./webpack.config.common');
 
 let distConfig = {
+	mode: "production",
 	entry: {
 		main: path.resolve(APP_ROOT, `src/main.js`)
 	},
@@ -63,19 +64,16 @@ let demoConfig = {
 		}),
 		/**
 		 * 生产环境
+		 * webpack 4 默认支持: 'process.env.NODE_ENV': JSON.stringify('production')
 		 */
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify('production'),
 			__DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
-		}),
-		/**
-		 * webpack3.x 模块串联
-		 */
-		new webpack.optimize.ModuleConcatenationPlugin()
+		})
 	],
 };
 
-module.exports = [ 
+module.exports = [
 	webpackMerge( commonConfig, distConfig ),
 	webpackMerge( commonConfig, demoConfig ),
 ];
